8e1a26d93ff402cea7c92aa0c86350a23326adb4
GUY YAO <guyyao003@gmail.com>
2024-02-12 20:26:34 +0000

    test02

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ clubs = loadClubs()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',clubs=clubs,competitions=competitions)
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
